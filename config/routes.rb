bf2f08214a1948e39879c2f3951028eb6a3f8340
ChocolateCharlie <aurore.amrit@gmail.com>
2018-07-20 22:29:30 +0200

    Add a form on the show page to delete the given book

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   get 'books/:id' => 'books#show'
   patch 'books/:id' => 'books#update'
+  delete 'books/:id' => 'books#destroy'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
