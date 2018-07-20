bd2dd7069a2d14626e383327c202cb2b73377d8a
ChocolateCharlie <aurore.amrit@gmail.com>
2018-07-20 22:38:11 +0200

    Create the table categories and index it to books

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_20_190236) do
+ActiveRecord::Schema.define(version: 2018_07_20_203055) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
   end
 
 end
