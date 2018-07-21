4e9c7c0c2858a728da11655029d76dfadd5374b9
ChocolateCharlie <aurore.amrit@gmail.com>
2018-07-21 11:39:43 +0200

    Add a scope for category to both forms

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,19 +1,21 @@
 class BooksController < ApplicationController
   def index
     @books = Book.all
+    @categories = Category.all
   end
 
   def create
-    Book.create title: params[:title]
+    Book.create title: params[:title], category_id: params[:category_id]
     redirect_to "/books"
   end
 
   def show
+    @categories = Category.all
     @book = Book.find(params[:id])
   end
 
   def update
-    Book.find(params[:id]).update title: params[:title]
+    Book.find(params[:id]).update title: params[:title], category_id: params[:category_id]
     redirect_to "/books/#{params[:id]}"
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_20_203055) do
+ActiveRecord::Schema.define(version: 2018_07_20_203416) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
+    t.integer "category_id"
+    t.index ["category_id"], name: "index_books_on_category_id"
   end
 
   create_table "categories", force: :cascade do |t|
