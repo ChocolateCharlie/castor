91bf1f0b94ac4831762116ac38074c6f3922a143
ChocolateCharlie <aurore.amrit@gmail.com>
2018-07-21 22:13:08 +0200

    Add some validators and some scopes to the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,17 @@
 class User < ActiveRecord::Base
+
+  validates :name, presence: {
+    message: "Le nom doit être renseigné."
+  }
+
+  validates :name, uniqueness: {
+    message: "Ce nom est déjà pris."
+  }
+
+  validates :password, presence: {
+    message: "Le mot de passe doit être renseigné."
+  }
+
+  scope :admin, -> { where(role: "admin") }
+  scope :user, -> { where(role: "user") }
 end
