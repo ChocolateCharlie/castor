0d7fa542177b806409255b6a3fc955f3e91e9747
ChocolateCharlie <aurore.amrit@gmail.com>
2018-07-21 23:06:46 +0200

    Style the connect/disconnect link and the flash messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,11 +3,11 @@ class UsersController < ApplicationController
     @current_user = User.where(name: params[:name], password: params[:password]).first
     if @current_user
       session[:user_id] = @current_user.id
-      flash[:info] = "Bienvenue #{@current_user.name} !"
+      flash[:success] = "Bienvenue #{@current_user.name} !"
       redirect_to "/books"
     else
       session[:user_id] = nil
-      flash[:info] = "La connexion a échoué."
+      flash[:failure] = "La connexion a échoué."
       redirect_to "/users/login"
     end
   end
