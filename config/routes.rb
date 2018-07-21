30da1f41fbae27ac5c45451c0fa04be35fbaaa8c
ChocolateCharlie <aurore.amrit@gmail.com>
2018-07-21 17:40:57 +0200

    Simplify routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
-  get 'books' => 'books#index'
-  post 'books' => 'books#create'
-
-  get 'books/:id' => 'books#show'
-  patch 'books/:id' => 'books#update'
-  delete 'books/:id' => 'books#destroy'
+  resources :books, only: [:index, :create, :show, :update, :destroy]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
