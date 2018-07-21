d6cf6b119d0a281252bf4f73e63bb7e68a55821c
ChocolateCharlie <aurore.amrit@gmail.com>
2018-07-21 17:29:38 +0200

    Create book_link helper

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -1,2 +1,11 @@
 module BooksHelper
+  def book_link book
+    html = "<a href='/books/#{book.id}'>".html_safe
+    html += book.title
+    if book.category
+      html += ' (' + book.category.name + ')'
+    end
+    html += "</a>".html_safe
+    html
+  end
 end
