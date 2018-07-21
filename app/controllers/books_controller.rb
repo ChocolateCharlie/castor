08d1253500ec68b753db4b6097f2528e6c2c44fb
ChocolateCharlie <aurore.amrit@gmail.com>
2018-07-21 17:38:47 +0200

    Show flash messages on book creation, update and deletion

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -12,8 +12,10 @@ class BooksController < ApplicationController
     @book.title = params[:title]
     @book.category_id = params[:category_id]
     if @book.save
+      flash[:success] = "Le livre a été créé."
       redirect_to "/books"
     else
+      flash[:failure] = "Le livre n'a pas pu être créé."
       render 'index'
     end
   end
@@ -26,17 +28,20 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     if @book.update title: params[:title], category_id: params[:category_id]
+      flash[:success] = "Le livre a été mis à jour."
       redirect_to "/books/#{params[:id]}"
     else
       @categories = Category.all
       @book.title = Book.find(params[:id]).title
       @book.category = Book.find(params[:id]).category
+      flash[:failure] = "Le livre n'a pas été mis à jour."
       render 'show'
     end
   end
 
   def destroy
     Book.find(params[:id]).destroy
+    flash[:success] = "Le livre a été supprimé."
     redirect_to "/books"
   end
 end
